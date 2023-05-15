b81018f5cb4b6c976fde635d90d9127bb0ecb80f
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2023-05-15 00:12:45 -0600

    PortalsApp actualizado

--- src/apps/portals/PortalsApp.jsx
+++ src/apps/portals/PortalsApp.jsx
@@ -1,11 +1,22 @@
 import { Component } from 'react'
 import ReactDOM from 'react-dom'
 
+import PropTypes from 'prop-types'
+
 
 class ModalContainer extends Component {
+
+
+    static propTypes = {
+        isModalOpen: PropTypes.bool,
+        children: PropTypes.node
+    }
+
     render(){
 
-
+        if( ! this.props.isModalOpen ){
+            return null
+        }
 
         return ReactDOM.createPortal((
             <div
@@ -42,12 +53,42 @@ class ModalContainer extends Component {
 
 export class PortalsApp extends Component {
 
+
+
+    state = {
+        isModalOpen: false
+    }
+
+    openModal = () => {
+        this.setState({
+            isModalOpen: true
+        })
+    }
+
+    closedModal = () => {
+        this.setState({
+            isModalOpen: false
+        })
+    }
+
     render() {
         return (
             <div>
-                <p>Portals-App</p> 
-                <ModalContainer>
+                <p>Portals App</p>
+                <button 
+                    onClick={ this.openModal }
+                >
+                    Abrir modal
+                </button> 
+                
+                
+                <ModalContainer isModalOpen={ this.state.isModalOpen}>
                     <p>Contenido modal</p>
+                    <button 
+                        onClick={ this.closedModal }
+                    >
+                        Cerrar modal
+                    </button> 
                 </ModalContainer>
             </div>
         )
